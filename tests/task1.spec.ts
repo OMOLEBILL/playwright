965fcc7899cb46e97f5167abf8810783b594f20a
bill omole <omolebill01@gmail.com>
2024-12-05 12:58:57 +0100

    Organized the test actions using test.step

--- tests/task1.spec.ts
+++ tests/task1.spec.ts
@@ -1,22 +1,27 @@
 import { test, expect } from '@playwright/test';
 test.describe('Sauce Demo Site Automation', () => {
-    // Task 1: Locked Out User Test
-    test('Locked out user login attempt', async ({ page }) => {
-      await page.goto('https://www.saucedemo.com/');
-  
-      // Enter credentials
-      await page.fill('input[name="user-name"]', 'locked_out_user');
-      await page.fill('input[name="password"]', 'secret_sauce');
-      await page.click('input[name="login-button"]');
-  
-      // Check that user is on the login page
-      await expect(page.locator('.login_logo')).toBeVisible();
-  
-      // Check for warning message
-      const warningMessage = page.locator('.error-message-container');
-      await expect(warningMessage).toContainText('Epic sadface: Sorry, this user has been locked out.');
-  
-      // Close the warning message
-      await page.click('.error-button');
+    test('Login with a locked out user', async ({ page }) => {
+      await test.step('Navigate to the login page', async () => {
+          await page.goto('https://www.saucedemo.com/');
+      });
+    
+      await test.step('Enter credentials', async () => {
+        await page.fill('input[name="user-name"]', 'locked_out_user');
+        await page.fill('input[name="password"]', 'secret_sauce');
+        await page.click('input[name="login-button"]');
+      });
+    
+      await test.step('Check that user is on the login page', async () => {
+        await expect(page.locator('.login_logo')).toBeVisible();
+      });
+    
+      await test.step('Check for warning message', async () => {
+        const warningMessage = page.locator('.error-message-container');
+        await expect(warningMessage).toContainText('Epic sadface: Sorry, this user has been locked out.');
+      });
+    
+      await test.step('Close the warning message', async () => {
+        await page.click('.error-button');
+      });
     });
 });
